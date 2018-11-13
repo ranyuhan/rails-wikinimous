c9220596391414d095eafe2644550fbafcd00708
AveryJin <avery.jin@gmail.com>
2018-11-13 15:31:12 +0800

    done wikinimous without styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article = Article.create({title: Faker::Pokemon.name, content: Faker::StarWars.quote})
+end
+puts "#{Article.count} articles has been created!"
